4f204e8f5ce8145101b8ce35fdf02b15ed906eeb
Ahmadi Miyed Niloy <119941357+Miyed-web@users.noreply.github.com>
2024-02-24 15:08:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const section = document.querySelector("section");
+window.addEventListener("scroll",( )=>{
+  let value = window.scrollY;
+  section.style.clipPath = "circle(" + value +"px at left center)"
+})
